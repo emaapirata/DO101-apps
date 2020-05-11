cf6ee6a0ffed695063167df4f2855d782e854096
emaapirata <emaapirata@gmail.com>
2020-05-11 16:39:42 -0300

    v2

--- weather/routes/index.js
+++ weather/routes/index.js
@@ -13,15 +13,16 @@ router.get('/', function(req, res) {
 router.post('/get_weather', async function (req,res) {
   let city = req.body.city;
   let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${UNITS}&appid=${OWM_API_KEY}`;
-  console.log(OWM_API_KEY);
   try {
     let data = await fetch(url);
     let weather = await data.json();
     console.log(weather);
+    console.log(OWM_API_KEY);
     if(weather.cod == '404' && weather.main == undefined) {
       res.render('index', {weather: null, error: 'Error: Unknown city'});
     }
     else if (weather.cod == '401' && weather.main == undefined) {
+      console.log(OWM_API_KEY);
       res.render('index', {weather: null, error: 'Error: Invalid API Key. Please see http://openweathermap.org/faq#error401 for more info.'});
     }
     else {
